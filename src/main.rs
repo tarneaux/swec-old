45c3d98893052deef439058cc0609fc0baf7964d
tarneaux <tarneo@tarneo.fr>
2023-04-19 20:12:19 +0200

    Move monitor.rs to watcher.rs

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
-mod monitor;
-use monitor::{OKWhen, ServiceWatcher};
+mod watcher;
 use std::time::Duration;
 use tokio::time::sleep;
+use watcher::{OKWhen, ServiceWatcher};
 
 #[tokio::main]
 async fn main() {
     let mut watcher = ServiceWatcher::new(
-        "http://github.com/tarneaarstarst/",
+        "http://github.com/tarneaux/",
         Duration::from_secs(5),
-        OKWhen::Status(200),
+        OKWhen::InDom("supersplit".to_string()),
     );
 
     loop {
