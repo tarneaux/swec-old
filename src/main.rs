b43624acee501ddbb80f25fda6e2eb8488d553ba
tarneo <tarneo@renn.es>
2023-05-14 16:10:06 +0200

    Added some basic tests

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,10 @@ use crate::watcher::Status;
 #[tokio::main]
 async fn main() {
     let arguments = cli_handler::Arguments::parse();
-    let pond = ServiceWatcherPond::new_from_stdin().unwrap();
+    let pond = ServiceWatcherPond::new_from_stdin().unwrap_or_else(|e| {
+        eprintln!("Error: {:?}", e);
+        exit(1);
+    });
     match arguments.interval {
         Some(interval) => loop {
             let start_time = std::time::Instant::now();
@@ -32,11 +35,11 @@ async fn main() {
 
 async fn run_once(arguments: &Arguments, pond: &ServiceWatcherPond) -> Result<(), PondWorkerError> {
     let output: Vec<NamedWatcherStatus> = pond.run(arguments.timeout).await?;
-    print_output(output).await;
+    print_output(output);
     Ok(())
 }
 
-async fn print_output(output: Vec<NamedWatcherStatus>) {
+fn print_output(output: Vec<NamedWatcherStatus>) {
     for status in output {
         println!(
             "{} {}",
@@ -50,5 +53,44 @@ async fn print_output(output: Vec<NamedWatcherStatus>) {
                 },
             }
         );
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::watcher::{OKWhen, ServiceWatcher};
+    use cli_handler::Arguments;
+    use multi_watcher::ServiceWatcherPond;
+    use std::time::Duration;
+
+    #[tokio::test]
+    async fn test_run_once() {
+        let arguments = Arguments {
+            timeout: Duration::from_secs(5),
+            interval: None,
+        };
+        let mut pond = ServiceWatcherPond::new();
+        pond.add_watcher(
+            "google".to_string(),
+            ServiceWatcher::new("https://google.com", OKWhen::Status(200)),
+        )
+        .unwrap();
+        run_once(&arguments, &pond).await.unwrap();
+    }
+
+    #[test]
+    fn test_print_output() {
+        let output = vec![
+            NamedWatcherStatus {
+                name: "google".to_string(),
+                status: Status::Online(Duration::from_secs(0)),
+            },
+            NamedWatcherStatus {
+                name: "google".to_string(),
+                status: Status::Offline(watcher::ErrorType::Timeout),
+            },
+        ];
+        print_output(output);
     }
 }
