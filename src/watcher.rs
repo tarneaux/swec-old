87392a0ff68a63cdb4687b7cd1227f182a2dc019
tarneo <tarneo@tarneo.fr>
2023-07-09 15:10:52 +0200

    feat(watcher): allow regex-matching content

--- src/watcher.rs
+++ src/watcher.rs
@@ -4,6 +4,7 @@
  * License: GPLv2
  */
 
+use regex::Regex;
 use reqwest::Client;
 use serde::{Deserialize, Serialize};
 use std::time::Duration;
@@ -56,14 +57,17 @@ impl ServiceWatcher {
                 return Some(ErrorType::WrongStatus);
             }
         }
+        let body = res.text().await.unwrap_or_else(|e| {
+            eprintln!("Error while reading response body: {}", e);
+            String::new() // Check will fail because we search in an empty string
+        });
         if let Some(content) = &self.ok_when.content {
-            let body = res.text().await.unwrap_or_else(|e| {
-                eprintln!("Error while reading response body: {}", e);
-                String::new() // Check will fail because we search in an empty string
-            });
             if !body.contains(content) {
                 return Some(ErrorType::WrongContent);
             }
+        }
+        if !self.ok_when.content_regex.is_match(&body) {
+            return Some(ErrorType::WrongContent);
         }
         None
     }
@@ -89,6 +93,12 @@ pub struct OkWhen {
     #[serde(default = "default_ok_status")]
     status: Option<u16>,
     content: Option<String>,
+    #[serde(
+        serialize_with = "regex_serialize",
+        deserialize_with = "regex_deserialize",
+        default = "default_ok_regex"
+    )]
+    content_regex: Regex,
 }
 
 impl Default for OkWhen {
@@ -96,10 +106,30 @@ impl Default for OkWhen {
         Self {
             status: default_ok_status(),
             content: None,
+            content_regex: default_ok_regex(),
         }
     }
 }
 
 const fn default_ok_status() -> Option<u16> {
     Some(200)
+}
+
+fn default_ok_regex() -> Regex {
+    Regex::new("").unwrap()
+}
+
+fn regex_serialize<S>(regex: &Regex, serializer: S) -> Result<S::Ok, S::Error>
+where
+    S: serde::Serializer,
+{
+    serializer.serialize_str(regex.as_str())
+}
+
+fn regex_deserialize<'de, D>(deserializer: D) -> Result<Regex, D::Error>
+where
+    D: serde::Deserializer<'de>,
+{
+    let s = String::deserialize(deserializer)?;
+    Regex::new(&s).map_err(serde::de::Error::custom)
 }
