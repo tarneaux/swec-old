208d1a297a29f18ca22a47736f5b39d59d4f9985
tarneo <tarneo@tarneo.fr>
2023-10-13 11:55:57 +0200

    Implement Debug and Display for status structs

--- src/watchers/status.rs
+++ src/watchers/status.rs
@@ -4,22 +4,43 @@
  * License: GPLv2
  */
 
+use core::fmt::{Display, Formatter};
 use serde::{Deserialize, Serialize};
 use std::time::Duration;
 use std::time::SystemTime;
 
-#[derive(Clone, Serialize, Deserialize)]
+#[derive(Clone, Serialize, Deserialize, Debug)]
 pub enum Status {
     Up(Duration),
     Down(DownReason),
 }
 
-#[derive(Clone, Serialize, Deserialize)]
+impl Display for Status {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Status::Up(duration) => write!(f, "Up: took {}", duration.as_secs()),
+            Status::Down(reason) => write!(f, "Down: {}", reason),
+        }
+    }
+}
+
+#[derive(Clone, Serialize, Deserialize, Debug)]
 pub enum DownReason {
     Timeout,
     WrongContent,
     WrongStatus,
     Unknown,
+}
+
+impl Display for DownReason {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            DownReason::Timeout => write!(f, "Timeout"),
+            DownReason::WrongContent => write!(f, "Wrong content"),
+            DownReason::WrongStatus => write!(f, "Wrong status"),
+            DownReason::Unknown => write!(f, "Unknown"),
+        }
+    }
 }
 
 #[derive(Clone, Serialize, Deserialize)]
