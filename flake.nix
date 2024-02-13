a8e15370c75037d40fa55400ed8d56acff006393
tarneo <tarneo@tarneo.fr>
2024-02-13 16:30:29 +0100

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, nixpkgs-unstable }: let
+    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    pkgs-unstable = nixpkgs-unstable.legacyPackages.x86_64-linux;
+  in {
+    devShells.x86_64-linux.default = pkgs.mkShell {
+      packages = [
+        pkgs.openssl
+        pkgs.pkg-config
+        pkgs-unstable.cargo
+        pkgs-unstable.rustc
+      ];
+      shellHook = ''
+        export PKG_CONFIG_PATH="${pkgs.openssl.dev}/lib/pkgconfig";
+      '';
+    };
+  };
+}
