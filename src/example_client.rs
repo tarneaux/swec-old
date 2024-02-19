80943809af5318c95b44ca18f883658948789a39
tarneo <tarneo@tarneo.fr>
2024-02-19 20:08:03 +0100

    Example_client: Add comments

--- src/example_client.rs
+++ src/example_client.rs
@@ -3,7 +3,10 @@ use swec::watcher::{Info, Status};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Create a new reqwest client
     let client = Client::new();
+
+    // Create a new watcher with a description and no URL
     let resp = client
         .post("http://localhost:8081/watchers/test/spec")
         .json(&Info::new("test".to_string(), None))
@@ -11,6 +14,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let resp = resp.await?;
     std::println!("{}", resp.status());
 
+    // Get the watcher's spec
     let resp = client
         .get("http://localhost:8081/watchers/test/spec")
         .send();
@@ -18,6 +22,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let resp = resp.json::<Info>().await?;
     std::println!("{:?}", resp);
 
+    // Add a status to the watcher
     let resp = client
         .post("http://localhost:8081/watchers/test/statuses")
         .json(&Status {
@@ -30,6 +35,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let resp = resp.await?;
     std::println!("{}", resp.status());
 
+    // Add multiple statuses to the watcher
     let resp = client
         .post("http://localhost:8081/watchers/test/statuses")
         .json(&vec![
@@ -48,6 +54,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let resp = resp.await?;
     std::println!("{}", resp.status());
 
+    // Get the watcher's statuses
     let resp = client
         .get("http://localhost:8081/watchers/test/statuses")
         .send();
