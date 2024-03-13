b775148b54e616b0f4572bf550a2ef198b5583ff
tarneo <tarneo@tarneo.fr>
2024-03-13 17:45:07 +0100

    core: Implement Display for ApiMessage

--- swec-core/src/api.rs
+++ swec-core/src/api.rs
@@ -1,6 +1,7 @@
 use crate::{checker, Spec};
 use chrono::{DateTime, Local};
 use serde::{Deserialize, Serialize};
+use std::fmt::{self, Display, Formatter};
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct Info {
@@ -14,4 +15,23 @@ pub enum Message {
     AddedStatus(DateTime<Local>, checker::Status),
     Initial(Spec, Option<(DateTime<Local>, checker::Status)>),
     CheckerDeleted,
+}
+
+impl Display for Message {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::UpdatedSpec(spec) => write!(f, "Updated spec: {spec}"),
+            Self::AddedStatus(time, status) => {
+                write!(f, "Added status at {time}: {status}")
+            }
+            Self::Initial(spec, status) => {
+                write!(f, "Initial spec: {spec}")?;
+                if let Some((time, status)) = status {
+                    write!(f, ", initial status at {time}: {status}")?;
+                }
+                Ok(())
+            }
+            Self::CheckerDeleted => write!(f, "Checker deleted"),
+        }
+    }
 }
